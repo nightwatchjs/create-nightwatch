bdae4f7b407c52513c3af38c9c470f0adbba80a5
Priyansh Garg <39924567+garg3133@users.noreply.github.com>
2022-09-22 02:22:00 +0530

    Add duckduckgo BDD example for TS. (#59)

--- tests/e2e_tests/init.js
+++ tests/e2e_tests/init.js
@@ -642,8 +642,8 @@ describe('e2e tests for init', () => {
     const examplesPath = path.join(rootDir, answers.examplesLocation);
     assert.strictEqual(fs.existsSync(examplesPath), true);
     const exampleFiles = fs.readdirSync(examplesPath);
-    assert.strictEqual(exampleFiles.length, 4);
-    assert.deepEqual(exampleFiles, ['ecosia.ts', 'github.ts', 'google.ts', 'tsconfig.json']);
+    assert.strictEqual(exampleFiles.length, 5);
+    assert.deepEqual(exampleFiles, ['duckDuckGo.ts', 'ecosia.ts', 'github.ts', 'google.ts', 'tsconfig.json']);
 
     // Test console output
     const output = consoleOutput.toString();
