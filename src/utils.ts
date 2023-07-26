dc9a60b733de771b1756fe70ac5c8f0bc36c43b0
Priyansh Garg <priyanshgarg30@gmail.com>
2023-07-26 14:04:52 +0530

    Remove rmdirSync from utils.

--- src/utils.ts
+++ src/utils.ts
@@ -2,19 +2,3 @@ import fs from 'node:fs';
 import path from 'path';
 
 export const isNodeProject = (rootDir: string): boolean => fs.existsSync(path.join(rootDir, 'package.json'));
-
-export const rmDirSync = (dirPath: string) => {
-  if (fs.existsSync(dirPath)) {
-    fs.readdirSync(dirPath).forEach((file) => {
-      const curPath = path.join(dirPath, file);
-      if (fs.lstatSync(curPath).isDirectory()) {
-        // recurse
-        rmDirSync(curPath);
-      } else {
-        // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(dirPath);
-  }
-};
